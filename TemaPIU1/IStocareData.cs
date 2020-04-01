c8bf78a5ba2856c003a42d76464dfa877cfbe5dd
Petru Alin Tanasa <petru.petru.ro@gmail.com>
2020-04-01 12:03:42 +0300

    lab5

---
+++ TemaPIU1/IStocareData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPIU1
+{
+   public interface IStocareData
+    {
+        void AddUtilizator(Utilizator u);
+        Utilizator[] GetUtilizator(out int nrUtilizatori);
+    }
+}
